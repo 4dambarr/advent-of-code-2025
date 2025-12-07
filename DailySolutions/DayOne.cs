a27cd8b930b7ea05882a016b5470ad86fa45a477
4dambarr <adam.barr@outlook.com>
2025-12-07 21:53:10 +0000

    solution: day 1 part 1 and 2

---
+++ DailySolutions/DayOne.cs
@@ -0,0 +1,43 @@
+namespace advent_of_code_2025.DailySolutions;
+
+public static class DayOne
+{
+    public static void PartOne()
+    {
+        var inputStrings = File.ReadAllLines("Assets/DayOnePassword.txt");
+        var zeroCount = 0;
+        var pos = 50;
+        foreach (var line in inputStrings)
+        {
+            var direction = line[0] == 'L' ? 1 : -1;
+            var rotation = int.Parse(line.Substring(1));
+            pos += (direction * rotation);
+            pos %= 100;
+            if (pos == 0) zeroCount++;
+        }
+        Console.WriteLine(zeroCount);
+        // Answer: 1152
+    }
+
+    public static void PartTwo()
+    {
+        var inputStrings = File.ReadAllLines("Assets/DayOnePassword.txt");
+        var zeroCount = 0;
+        var pos = 50;
+        foreach (var line in inputStrings)
+        {
+            var direction = line[0] == 'L' ? 1 : -1;
+            var rotation = int.Parse(line.Substring(1));
+            if (rotation >= 100)
+            {
+                zeroCount += (int)(rotation / 100);
+            }
+            var newPos = pos + (direction * (rotation % 100));
+            
+            if (newPos >= 100 || newPos <= -100 || (newPos > 0 && pos < 0) || (newPos < 0 && pos > 0) || newPos == 0) zeroCount++;
+            pos = newPos % 100;
+        }
+        
+        Console.WriteLine(zeroCount);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using advent_of_code_2025.DailySolutions;
+
+DayOne.PartTwo();
